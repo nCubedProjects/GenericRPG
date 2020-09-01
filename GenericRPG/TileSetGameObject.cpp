345c94f57267ee6d61e17027435234e5e8d85181
Trent Novelly <trentnov@gmail.com>
2020-09-01 08:53:02 -0500

    BUGFIX: Correctly resize the default object size of a tileset to be the size of one frame and not the whole texture

--- GenericRPG/TileSetGameObject.cpp
+++ GenericRPG/TileSetGameObject.cpp
@@ -14,10 +14,18 @@ TileSetGameObject::TileSetGameObject(SDL_Texture * texture,
 	int w, h;	//temp vars for a single frame's width and height in pixels.
 	SDL_QueryTexture(objTexture, NULL, NULL, &w, &h);
 
+	//calculate the size of one frame
 	w = w / num_across;
 	h = h / num_down;
 
-	
+	//set the default location width and height to be the size of one frame, not the whole texture
+	objLocation.w = w;
+	objLocation.h = h;
+
+
+	//initialize the frames
+		//each frame will have the same size
+		//but the origin will point to the top left corner of each frame
 	total_frames = num_across * num_down;
 	frames = (SDL_Rect*)malloc(total_frames * sizeof(SDL_Rect));
 	
@@ -30,6 +38,8 @@ TileSetGameObject::TileSetGameObject(SDL_Texture * texture,
 			frames[frame_index].h = h;
 		}
 	}
+
+	
 }
 
 void TileSetGameObject::Render(SDL_Renderer* renderer) {
