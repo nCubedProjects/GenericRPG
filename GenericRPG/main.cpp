040875690ef7a1ab625eaf90907750ac71707c98
Trent Novelly <trentnov@gmail.com>
2020-12-14 16:25:39 -0600

    lets get this documentation party started!!!!

--- GenericRPG/main.cpp
+++ GenericRPG/main.cpp
@@ -2,6 +2,15 @@
 
 #include "Runtime.h"
 
+/**
+ * @brief Program entry.
+ * 
+ * @details Initializes a game Runtime, and runs the basic gameloop
+ * 
+ * @param argc 
+ * @param args 
+ * @return 
+*/
 int main(int argc, char** args) {
 	Runtime* game_runtime = new Runtime();
 
